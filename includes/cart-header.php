38796c0225530c4c5259abba4cb65f54a2a060fe
Dara Amin <drasoulm@gmail.com>
2016-07-17 14:41:19 -0400

    trying soomething

--- includes/cart-header.php
+++ includes/cart-header.php
@@ -1,3 +1,3 @@
 <div class="cartMenu">
-    <?php wp_nav_menu( array( 'sort_column' => 'menu_order', 'container_class' => 'menu-header' ) ); ?>
+    <?php wp_nav_menu( array( 'menu' => 'Harvest Menu', 'sort_column' => 'menu_order', 'container_class' => 'menu-header' ) ); ?>
   </div>
