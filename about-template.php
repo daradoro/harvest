a1e07a84139c998d6172c271e95561a290af2e3a
Dara Amin <drasoulm@gmail.com>
2016-07-17 20:46:41 -0400

    added read more

--- about-template.php
+++ about-template.php
@@ -13,7 +13,6 @@
 
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/style.css" media="screen" />
   <link rel="stylesheet" type="text/css" href="<?php bloginfo('template_directory'); ?>/about-template.css" media="screen" />
-
 	<!-- Latest compiled and minified JavaScript -->
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 		<meta charset="<?php bloginfo('charset'); ?>">
@@ -28,17 +27,26 @@
 <div class="pageContainer">
   <div class="row wideAbout rowBanner">
   </div>
-<div class="container">
+<div class="container baskerVille">
   <div class="row">
          <?php
       if (have_posts()) : 
         while(have_posts()) : the_post(); ?>
 
-          <h2>TITLE</h2>
-          <h1><?php echo get_post_field('post_content', 'titleContent'); ?></h1>
-
+          <h2><?php the_field('first_header'); ?></h2>
+          <h1><?php the_field('second_header'); ?></h1>
+          <section>
+            <article>
+              <div class=" aboutCopyContainer container">
+                <div class="container">
+                    <img src="<?php the_field('first_image'); ?>">
+                    <h3><?php the_content(); ?></h3>
+                </div>
+              </div>
+            </article>
+          </section>
       <div>
-      <h2 class="baskerVilleItalic"><?php the_content(); ?></h2>
+      <!-- <h2 class="baskerVilleItalic"><?php the_content(); ?></h2> -->
       <?php  endwhile;
 
         else : 
@@ -58,5 +66,9 @@
   <br>
   <br>
 </div>
+  <script type="text/javascript">
+  $(document).ready(function(){
+  });
+  </script>
 <!-- /.container -->
 <?php get_footer(); ?>
--- functions.php
+++ functions.php
@@ -3,3 +3,5 @@
 my_function - brief line describing what it does
 ************************************************************************/
 add_theme_support( 'menus' );
+
+//wp_enqueue_script( 'script', get_template_directory_uri() . 'js/readmore.js', array ( 'jquery' ));
